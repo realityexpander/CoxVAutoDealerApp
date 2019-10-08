2e54dc630714bbe03f72ecb2d9a7153dc5d64297
realityexpander <realityexpander@gmail.com>
2019-10-08 18:54:28 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/marsrealestate/start/StartFragment.kt
@@ -0,0 +1,62 @@
+/*
+ *  Copyright 2019, The Android Open Source Project
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.example.android.marsrealestate.start
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProviders.*
+import androidx.navigation.fragment.findNavController
+import com.example.android.marsrealestate.databinding.FragmentStartBinding
+
+
+class StartFragment : Fragment() {
+
+    /**
+     * Lazily initialize our [StartViewModel].
+     */
+    private val viewModel: StartViewModel by lazy {
+        ViewModelProviders.of(this).get(StartViewModel::class.java)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val application = requireNotNull(activity).application
+        val binding = FragmentStartBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+
+        val viewModelFactory = StartViewModelFactory(application)
+        binding.viewModel = of(
+                this, viewModelFactory).get(StartViewModel::class.java)
+
+        // LoadDealerships.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_showOverview))
+        viewModel.navigateToOverview.observe(this, Observer {
+            if ( it != false ) {
+                this.findNavController().navigate(StartFragmentDirections.actionShowOverview())
+                viewModel.displayOverviewComplete()
+            }
+        })
+
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/android/marsrealestate/start/StartViewModel.kt
@@ -0,0 +1,46 @@
+/*
+ *  Copyright 2019, The Android Open Source Project
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.example.android.marsrealestate.start
+
+import android.app.Application
+import androidx.lifecycle.*
+
+class StartViewModel(/*marsProperty: MarsProperty,*/
+                     app: Application) : AndroidViewModel(app) {
+
+    // The internal MutableLiveData for the selected property
+    private val _navigateToOverview = MutableLiveData<Boolean?>()
+    val navigateToOverview: LiveData<Boolean?>
+        get() = _navigateToOverview
+
+    // Initialize the _selectedProperty MutableLiveData
+    init {
+        _navigateToOverview.value = false
+    }
+
+
+    fun displayOverview() {
+        _navigateToOverview.value = true
+    }
+
+    fun displayOverviewComplete() {
+        _navigateToOverview.value = false
+    }
+
+
+}
+
