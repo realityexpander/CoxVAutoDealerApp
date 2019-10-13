2294e7a62f9e483678601646aaefadefb979ed58
realityexpander <realityexpander@gmail.com>
2019-10-13 15:13:08 -0500

    use let and dealer properly

--- app/src/main/java/com/example/android/coxcardealer/dealers/DealersFragment.kt
+++ app/src/main/java/com/example/android/coxcardealer/dealers/DealersFragment.kt
@@ -50,7 +50,7 @@ class DealersFragment : Fragment() {
         // Observe the navigateToSelectedDealer LiveData and Navigate when it isn't null
         // After navigating, displayDealerComplete() will reset for future navigation.
         viewModel.navigateToSelectedDealer.observe(this, Observer { dealer ->
-          dealer.let {
+          dealer?.let {
             // Must find the NavController from the Fragment
             this.findNavController().navigate(DealersFragmentDirections.actionShowVehicles(dealer))
             viewModel.displayVehiclesComplete()
