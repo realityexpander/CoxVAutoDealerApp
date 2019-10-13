03bcb1382101ff3a7f91ac2e79e6d3d0ffa02e48
realityexpander <realityexpander@gmail.com>
2019-10-12 21:43:46 -0500

    Made more concurrent by calling DealerInfo immed. after VehicleInfo

--- app/src/main/java/com/example/android/coxcardealer/network/CarsApiService.kt
+++ app/src/main/java/com/example/android/coxcardealer/network/CarsApiService.kt
@@ -2,6 +2,7 @@ package com.example.android.coxcardealer.network
 
 import android.content.Context
 import android.os.Environment
+import androidx.core.os.EnvironmentCompat
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
@@ -9,12 +10,10 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import kotlinx.coroutines.Deferred
-import okhttp3.Cache
+import okhttp3.*
 import retrofit2.http.Path
-import okhttp3.OkHttpClient
-import okhttp3.ConnectionPool
-import okhttp3.Dispatcher
 import java.io.File
+import java.io.IOException
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
@@ -35,23 +34,52 @@ private val moshi = Moshi.Builder()
  * Use the Retrofit builder to build a retrofit object using a Moshi converter with our Moshi
  * object.
  */
+private val REWRITE_CACHE_CONTROL_INTERCEPTOR = Interceptor { chain ->
+    val originalResponse = chain.proceed(chain.request())
+    println("OriginalResponse : $originalResponse")
+    originalResponse.newBuilder()
+        .removeHeader("Pragma")
+        .addHeader("Cache-Control", String.format("max-age=%d", 60))
+        .build()
+}
 //val dispatcher: Dispatcher = Dispatcher(Executors.newFixedThreadPool(20)).apply {
 val dispatcher: Dispatcher = Dispatcher().apply {
     this.maxRequests = 40
     this.maxRequestsPerHost = 20
 }
-var cacheDir = File(Environment.getExternalStorageDirectory().path + "/cached_api2")
-//var cacheDir = File("/cache/cache_api")
-var pool = ConnectionPool(20, 5000, TimeUnit.MILLISECONDS)
+//var cacheDir = File(Environment.getDownloadCacheDirectory().path + "/cached_api3").also {
+//    if (!it.exists()) {  // ** testing
+//        it.mkdirs()
+//    }
+//}
+//var cacheDir = File(Environment.getExternalStorageDirectory().path + "/cached_api4").also {
+//    if (!it.exists()) {  // ** testing
+//        it.mkdirs()
+//    }
+//}
+var cacheDir = File(Environment.getDataDirectory().path + "/cache/cache_api5").also {
+    if(!it.exists()) {
+        it.mkdirs()
+    }
+}
+//var cacheDir = File("/cache2/cache_api6").also {
+//    if(!it.exists()) {
+//        it.mkdirs()
+//    }
+//}
+var pool = ConnectionPool(20, 1000, TimeUnit.MILLISECONDS)
 val client: OkHttpClient = OkHttpClient.Builder()
+    .addInterceptor(REWRITE_CACHE_CONTROL_INTERCEPTOR)
     .dispatcher(dispatcher)
     .connectionPool(pool)
-    .cache(Cache(
-        cacheDir,
-        10L * 1024L * 1024L // 1 MiB
-    ))
-    .build()
-
+//    .cache(Cache(
+//        cacheDir,
+//        10L * 1024L * 1024L // 1 MiB
+//    ))
+    .build().also {
+//        it.networkInterceptors().add(REWRITE_CACHE_CONTROL_INTERCEPTOR)
+        println("CacheDir : $cacheDir")
+    }
 
 private val retrofit = Retrofit.Builder()
     .client(client)
